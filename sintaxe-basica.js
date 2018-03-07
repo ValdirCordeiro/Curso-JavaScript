7fe9019cd81078452f26fd347d8ee86988c44240
Valdir Júnior Cordeiro Rocha <vjcordeiror@gmail.com>
2018-03-07 12:56:10 -0300

    Sintaxe básica

---
+++ sintaxe-basica.js
@@ -0,0 +1,35 @@
+//statemens terminam com ponto e virgula
+var x = 15;
+var y = 12;
+
+// Valores: literais e variáveis
+//Literais
+10;
+10.5;
+
+"Olá mundo";
+'Olá mundo';
+
+// Variáveis
+var x;
+
+// Operadores
+x = 10;
+
+10 + 5;
+20 - 10;
+40 * 5;
+20 / 10;
+
+// Expressões
+y + 10;
+
+// Comentários
+/*
+	Bloco de Comentário
+*/
+
+// Indentificadores - CamelCase - Só pode ser começado por Letra, UnderLine ou  $ (cifrão)
+ var sintaxeBasica;
+ var _sintaxeBasica;
+ var $sintaxeBasica;
