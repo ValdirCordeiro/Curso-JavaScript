5d4aa41a886bf9a14754c07017c1f00f6d171ce2
Valdir Júnior Cordeiro Rocha <vjcordeiror@gmail.com>
2018-03-08 19:30:16 -0300

    Strings

---
+++ funcoes-Uteis/strings.js
@@ -0,0 +1,23 @@
+var s = "The winter is coming. It's really coming!";
+
+console.log(s);
+
+console.log(s.length);
+
+console.log(s.indexOf("coming"));
+
+console.log(s.lastIndexOf("coming"));
+
+console.log(s.slice(4, 10));
+
+console.log(s.slice(4, -8));
+
+console.log(s.substr(4,13));
+
+console.log(s.replace("winter", "summer"));
+
+console.log(s.concat("!", "summer"));
+
+var s = "Ned,Jon,Robb,Sansa,Bran,Arya,Rickon";
+
+console.log(s.split(","));
